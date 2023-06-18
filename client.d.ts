a59e3977eb60952e2da5131f1f235cd009b2f520
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2023-06-18 17:24:18 +0300

    feat: added Astro integration with 'virtual' plugin

---
+++ client.d.ts
@@ -0,0 +1,7 @@
+declare module 'virtual:nanointl' {
+  type Locales<T extends ReadonlyArray<string>> = T
+  
+  export const locales: ReadonlySet<string>
+  export const defaultLocale: string
+  export const resources: Record<string, unknown>
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,40 @@
-export const counter = 1
+import type { AstroIntegration } from 'astro'
+import virtual from 'vite-plugin-virtual'
+
+type Shape = Readonly<[string, ...string[]]>
+interface UserOptions<T extends Shape> {
+  locales: T
+  defaultLocale?: T[number]
+}
+
+function importLocalesGlob() {
+  return import.meta.glob([
+    '/**/{locales,translations}/**/*.json',
+    '!/public/**/{locales,translations}/**/*.json',
+  ], { eager: true, import: 'default' })
+}
+
+export default function<const T extends Shape>({ locales, defaultLocale = locales.at(0)! }: UserOptions<T>): AstroIntegration {
+  return {
+    name: 'astro-nanointl',
+    hooks: {
+      'astro:config:setup': ({ updateConfig }) => {
+        updateConfig({
+          vite: {
+            plugins: [
+              virtual({
+                'virtual:nanointl': `
+                  export const locales = new Set(${JSON.stringify(locales)})
+                  export const defaultLocale = ${JSON.stringify(defaultLocale)}
+                  export const resources = ${JSON.stringify(
+                    importLocalesGlob(),
+                  )}
+                `,
+              }),
+            ],
+          },
+        })
+      },
+    },
+  }
+}
