1187648e80fd8a28a5d937bd987ee10c7a6ead0f
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2024-03-29 12:00:01 +0200

    fix: updated 'params' transformer with dependencies removed

--- src/transformers/index.ts
+++ src/transformers/index.ts
@@ -1,1 +1,2 @@
 export { count } from './count'
+export { params } from './params'
---
+++ src/transformers/params.ts
@@ -0,0 +1,20 @@
+import type { TransformFunction } from './types'
+
+type ExtractParams<T extends string> = T extends `${infer Prefix}{${infer Param}}${infer Suffix}`
+  ? ({ [key in Param]: string } & ExtractParams<Prefix> & ExtractParams<Suffix>)
+  // eslint-disable-next-line ts/ban-types
+  : {}
+
+export function params<T extends string>(input: T): TransformFunction<T, ExtractParams<T>> {
+  return (_locale, translation = input) => {
+    return (params) => {
+      let result = translation.toString()
+
+      Object.keys(params).forEach(key =>
+        result = translation.replaceAll(`{${key}}`, (params as Record<string, string>)[key]),
+      )
+
+      return result
+    }
+  }
+}
